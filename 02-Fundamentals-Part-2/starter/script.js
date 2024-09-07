d67531e511cf95b3a0f5e1ef8347637633399372
fFukurou <supevi1@gmail.com>
2024-09-06 22:50:15 -0300

    Array methods, loops

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -91,8 +91,117 @@ const friends = ["Michael", "Kratos", "Frieren"];
 friends[0] = "Jay";
 console.log(friends);
 
-const fukurou = ["Fukurou", 20, "Student", "Depressed", 2025 - 2005, friends];
+let fukurou = ["Fukurou", 20, "Student", "Depressed", 2025 - 2005, friends];
 console.log(fukurou);
 
 const ages = [calcAge1(years[0]), calcAge1(years[2]), calcAge1(years[years.length-1])]
 console.log(ages);
+
+// ARRAY METHODS
+
+// will add to the end of the array
+friends.push("Monarch");
+
+// will add to the start of the array
+friends.unshift("Prez");
+
+// remove last element from an array
+friends.pop();
+
+// remove first element
+friends.shift();
+
+console.log(friends);
+
+// Getting index from an element
+// elements that do not exist in the array will return -1
+console.log(friends.indexOf('Kratos'));
+
+if (friends.includes('Prez')) {
+    console.log(`You have a friend called Prez!`);
+} else {
+    console.log(`Prez is not your friend... :(`);
+}
+
+// OBJECTS
+
+let fFukurou = {
+    firstName: "Fukurou",
+    lastName: "Sakai",
+    age: 2300 - 2280,
+    job: 'unemployed',
+    friends: friends,
+};
+
+const nameKey = "Name";
+
+console.log(fFukurou['age']);
+console.log(fFukurou['first' + nameKey]);
+console.log(fFukurou['last' + nameKey]);
+
+fFukurou.location = "Japan";
+fFukurou['status'] = "coding";
+
+// const answer = prompt("What do you want to know? age, firstName, lastName, job, friends, status, location?");
+
+// if (fFukurou[answer] != undefined) {
+//     console.log(fFukurou[answer]);
+
+// } else {
+//     console.log("Invalid answer");
+// }
+
+console.log(`${fFukurou.firstName} has ${fFukurou.friends.length} friends, and his best friend is ${fFukurou.friends[2]}`);
+
+fFukurou = {
+    firstName: "Fukurou",
+    lastName: "Sakai",
+    birthYear: 2004,
+    job: 'unemployed',
+    friends: friends,
+    isHappy: false,
+
+    // calcAge4: function() {
+    //     console.log(this);
+    //     return 2024 - this.birthYear;
+    // }
+    calcAge4: function(){
+        this.age = 2024 - this.birthYear;
+        return this.age;
+    }
+};
+
+console.log(fFukurou.calcAge4());
+console.log(fFukurou.age);
+
+console.log(`${fFukurou.firstName} is a ${fFukurou.age}-year old student, and he ${fFukurou.isHappy ? 'is happy' : 'is sad'}.`);
+
+// LOOPS
+
+// for (let i = 0; i < 3; i++) {
+//     console.log(i);
+// }
+
+console.log("---------------ARRAYS-------------------")
+
+const fukurouArray = ["Fukurou", 20, "Student", "Depressed", 2025 - 2005, friends];
+
+fukurouArray.forEach(element => {
+    console.log(element, typeof element);
+});
+
+for (let i = 0; i < fukurouArray.length; i++) {
+    if (typeof fukurouArray[i] === 'string') {
+        continue;
+    }
+    console.log(fukurouArray[i]);
+}
+
+const dummyArray = [2020, 20224, 2101, 39022, "3812190", 231, 10 , 21921 , 21321, 432]
+
+for (let i = 0; i < dummyArray.length; i++) {
+    if (typeof dummyArray[i] === 'string') {
+        break;
+    }
+    console.log(dummyArray[i]);
+}
