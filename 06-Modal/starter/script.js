ca4212d260903e4fbfe858a62ca11b7acbc44331
fFukurou <supevi1@gmail.com>
2024-09-10 23:43:46 -0300

    Modal Window, adding and removing classes

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+function openModal () {
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+
+};
+
+function closeModal () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+    btnsOpenModal[i].addEventListener('click', openModal);
+    // console.log("Button clicked");
+};
+
+
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
